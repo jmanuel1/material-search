847bf704aa79459449d085fbc78bfba05421fd4d
Jason Manuel <jama.indo@hotmail.com>
2019-07-04 21:21:38 +0000

    Add test task to gulpfile
    The tests requires a chromedriver file and a Selenium standalone .jar to
    be in the same directory as the tests. They are written with Python and
    use pytest and the Selenium Python bindings.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp   = require('gulp');
 var yarn   = require('gulp-yarn');
 var del    = require('del');
+var spawn  = require(/*'gulp-spawn'*/'child_process').spawn;
 
 gulp.task('build', gulp.series(function() {
   // Ensure that packages are installed.
@@ -18,3 +19,20 @@ gulp.task('build', gulp.series(function() {
   return gulp.src(['./node_modules/@bower_components/**'])
     .pipe(gulp.dest('./build/node_modules/@bower_components/'));
 }));
+
+var pythonProcess;
+gulp.task('test', gulp.series(
+  async function() { // Don't wait for exit, so async is used
+    // Assume build has been run
+    pythonProcess = spawn('python', ['-m', 'http.server'], {
+      cwd: './build'
+    });
+    return pythonProcess;
+  }, function() {
+    return spawn('pytest', ['test.py'], {
+      stdio: 'inherit', cwd: './test'
+    });
+  }, async function() {
+    return pythonProcess.kill();
+  })
+);
