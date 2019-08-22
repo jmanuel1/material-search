004f7284ef5e3868a8ebb48244ba6c1dc9f09717
Jason Manuel <jama.indo@hotmail.com>
2019-08-21 22:49:28 -0700

    Publish v0.1.1

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,10 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
+/* Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
+  This code may only be used under the BSD style license found at
+  http://polymer.github.io/LICENSE.txt The complete set of authors may be found
+  at http://polymer.github.io/AUTHORS.txt The complete set of contributors may
+  be found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by
+  Google as part of the polymer project is also subject to an additional IP
+  rights grant found at http://polymer.github.io/PATENTS.txt */
 
 (function(document) {
   'use strict';
@@ -27,7 +26,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
   // See https://github.com/Polymer/polymer/issues/1381
   // window.addEventListener('WebComponentsReady', function() {
-  //   // imports are loaded and elements have been registered
+  //    imports are loaded and elements have been registered
   // });
 
   // Close drawer after menu item is selected if drawerPanel is narrow
@@ -38,7 +37,10 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     }
   };
 
-  app._createItems = function (search) {
-    return search ? ["Fake result", "Yet another fake result", "So many fakes!"] : [];
-  }
+  app._createItems = function(search) {
+    if (search) {
+      return ['Fake result', 'Yet another fake result', 'So many fakes!'];
+    }
+    return [];
+  };
 })(document);
