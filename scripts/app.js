f1aacb16aa835160c6236c6ebd4b477270ccbf2c
Jason Manuel <jama.indo@hotmail.com>
2019-08-23 13:20:51 -0700

    Upgrade Polymer to 2.0
    The tests pass, but the website's appearance is broken.

--- scripts/app.js
+++ scripts/app.js
@@ -7,31 +7,26 @@
   rights grant found at http://polymer.github.io/PATENTS.txt */
 
 (function(document) {
-  'use strict';
+  "use strict";
 
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
+  var app = document.querySelector("#app");
 
   app.displayInstalledToast = function() {
-    document.querySelector('#caching-complete').show();
+    document.querySelector("#caching-complete").show();
   };
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
+  app.addEventListener("dom-change", function() {
+    console.log("Our app is ready to rock!");
   });
-
-  // See https://github.com/Polymer/polymer/issues/1381
-  // window.addEventListener('WebComponentsReady', function() {
-  //    imports are loaded and elements have been registered
-  // });
 
   // Close drawer after menu item is selected if drawerPanel is narrow
   app.onMenuSelect = function() {
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
+    var drawerPanel = document.querySelector("#paperDrawerPanel");
     if (drawerPanel.narrow) {
       drawerPanel.closeDrawer();
     }
@@ -39,7 +34,7 @@
 
   app._createItems = function(search) {
     if (search) {
-      return ['Fake result', 'Yet another fake result', 'So many fakes!'];
+      return ["Fake result", "Yet another fake result", "So many fakes!"];
     }
     return [];
   };
